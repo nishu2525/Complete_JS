df5512cd50b9a2319c6b633ed034ec12cf046023
Nishad Umap <131019693+nishu2525@users.noreply.github.com>
2024-04-25 13:54:31 +0000

    memory understanding

---
+++ Basics/4_memory.js
@@ -0,0 +1,28 @@
+/*
+Two types of data-- primitive type  & non-primitive type
+
+Two types of memory -- stack & Heap
+In primitive type stack memory is used - you get copy of declared variable
+In non-primitive type Heap memory is used - you get reference of Original value 
+
+ */
+// eg:-
+//----------------- Primitive Type ---------------------
+let myName="Nishad";
+let secondName=myName;// Here You only get copy of myName not original Value
+
+secondName="Umap"
+console.log(myName) //Nishad    
+console.log(secondName); // Umap
+
+//----------------- Non-Primitive Type ---------------------
+let user={
+    email:"user@gmail.com",
+    num:987456321
+}
+
+let usertwo=user;  // Here You get refernce of user 
+usertwo.email="usertwo@gmail.com"
+
+console.log(user.email);//usertwo@gmail.com
+console.log(usertwo.email); //usertwo@gmail.com
