e9fcd47e68218bec5aa0086585d99e805e13cac8
Nishad Umap <131019693+nishu2525@users.noreply.github.com>
2024-05-03 04:48:52 +0000

    call stack

--- part4/1_Execution_context.js
+++ part4/1_Execution_context.js
@@ -47,4 +47,39 @@ let result2= addNum(10,3)     // undefiened      // As same a aBove Functn
 console.log(result1); // 15
 console.log(result2); // 13
                 
-                
+         
+
+
+
+/*
+Call Stack --
+         1.  Global execution - then for all functions a new block is created, after work is done it(new block) get's deleted
+         when there are many functions each function executes as per line by line 
+
+         Now what if There is 1 function inside it another function is called and another one function is called inside it.
+         Eg:-
+                function one(){
+                    two(
+                        three()
+                    )
+                }
+        Then here which function comes out of Global execution  conetxt --- so here comes LIFO---- i.e last in first out 
+
+*/
+
+//Code____ Example
+function one(){
+        console.log("one");
+       
+    }
+    function two(){
+        console.log("two");
+       
+    }
+    function three(){
+        console.log("three");
+    }
+    one()
+     two()
+     three()
+    
